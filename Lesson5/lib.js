b2db0059221bb5055ef7b7e00be7d2dd5670e65f
c0balt <c0balt@yandex.ru>
2018-10-26 15:33:53 +0300

    1

---
+++ Lesson5/lib.js
@@ -0,0 +1,17 @@
+var start = (function () {
+  // debugger;
+  var random = Math.round(Math.random() * 1000);
+  var input = 0;
+  var count = 0;
+
+  return function () {
+    do {
+      input = prompt('Enter number: ');
+      count++;
+      alert(count);
+      if (count === 10) {
+        return input = null
+      };
+    } while (input != null);
+  }
+})();
