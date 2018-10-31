e6b6aa25a9b575edfe7074d7c513871c8d3eeed4
c0balt <c0balt@yandex.ru>
2018-10-31 17:34:48 +0300

    1

--- Lesson5/lib.js
+++ Lesson5/lib.js
@@ -2,15 +2,16 @@ var start = (function () {
   // debugger;
   var random = Math.round(Math.random() * 1000);
   var input = 0;
-  var count = 0;
+  var count = 10;
 
   return function () {
+    var flag = true;
     do {
       input = prompt('Enter number: ');
-      count++;
+      count--;
       console.log('осталось попыток: ' + count);
-      if (count === 10) {
-        return input = null
+      if (count === 0) {
+        return input = null;
       }
     }
     while (input != null);
