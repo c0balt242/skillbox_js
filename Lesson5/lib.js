1debba16d2c6f76d76a35caac2371d95176e1a6c
c0balt242 <43532677+c0balt242@users.noreply.github.com>
2018-11-01 21:12:34 +0300

    1

--- Lesson5/lib.js
+++ Lesson5/lib.js
@@ -1,18 +1,39 @@
 var start = (function () {
   // debugger;
-  var random = Math.round(Math.random() * 1000);
+  var random = Math.round(Math.random() * 10);
   var input = 0;
-  var count = 0;
+  var count = 10;
+
+  function check(input) {
+    return !input || isNaN(input);
+  }
+
+  function compare(input, random) {
+    if (input < random) {
+      alert('Это число меньше. Осталось попыток: ' + count);
+      return false;
+    } else if (input > random) {
+      alert('Это число больше. Осталось попыток: ' + count);
+      return false;
+    } else {
+      alert('Вы угадали за ' + (10 - count) + ' попыток!');
+      return true;
+    }
+  }
 
   return function () {
     do {
-      input = prompt('Enter number: ');
-      count++;
-      console.log('осталось попыток: ' + count);
-      if (count === 10) {
-        return input = null
+      input = prompt('Угадайте число от 1 до 10: ');
+      count--;
+      console.log('осталось попыток: ' + count + ' (' + random + ')');
+      if (count === 0) {
+        alert('Попытки закончились');
+        return input = null;
+      } else if (check(input)) {
+        alert('Это не число!');
+      } else if (compare(input, random)) {
+        break;
       }
-    }
-    while (input != null);
+    } while (input != null);
   }
 })();
