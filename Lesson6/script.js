28383c1617ef179ed28c4bc2da195242e3cae8b0
c0balt242 <43532677+c0balt242@users.noreply.github.com>
2018-11-15 23:07:59 +0300

    1

--- Lesson6/script.js
+++ Lesson6/script.js
@@ -1,1 +1,35 @@
-"use strict";
+// "use strict";
+
+var User = function (firstName, lastName, regDate) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.regDate = regDate;
+};
+
+// var UserList = function(user) {
+//   var users = [];
+//   var addUser = function(user) {
+//     users.push(user);
+//   };
+//   var getAllUsers = function() {
+//     return users;
+//   };
+// };
+
+function createUser(input) {
+  var u = input;
+  return u;
+}
+
+var date = new Date();
+
+var users = [];
+
+var user = new User('Anatoliy', 'Ivanov', date.toString());
+users.push(user);
+var user = new User('Vasya', 'Petrov', date.toString());
+users.push(user);
+var user = new User('Gena', 'Kakashkin', date.toString());
+users.push(user);
+console.log(user);
+console.log(users);
