c81feaadd2bf7a0533939dbd97f857f734392ab0
c0balt <c0balt@yandex.ru>
2018-11-20 17:27:56 +0300

    1

--- Lesson6/script.js
+++ Lesson6/script.js
@@ -1,29 +1,32 @@
 "use strict";
 
-var User = function (firstName, lastName, regDate) {
+var User = function(firstName, lastName, regDate) {
   this.firstName = firstName;
   this.lastName = lastName;
   this.regDate = regDate;
 };
 
-var UserList = function(User) {
+var UserList = function() {
   this.users = [];
-  
+
   this.add = function() {
     this.users.push(user);
-  }
-  
+  };
+
   this.show = function() {
-    return this.users;
-  }
-}
+    for (var i = 0; i <= this.users.length; i++) {
+      return this.users[i].firstName + ' ' + this.users[i].lastName + ' ' + this.users[i].regDate;
+    }
+  };
+};
 
 var date = new Date();
 var list = new UserList();
 
-var user = new User('Anatoliy', 'Ivanov', date.toString());
+var user = new User("Anatoliy", "Ivanov", date.toString());
 list.add(user);
-var user = new User('Petya', 'Kakashkin', date.toString());
+var user = new User("Petya", "Kakashkin", date.toString());
+list.add(user);
+var user = new User('Vasya', 'Zalupkin', date.toString());
 list.add(user);
 console.log(list.show());
-
