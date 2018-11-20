3f15d2b5de55b190163f027505f89891c29a0804
c0balt <c0balt@yandex.ru>
2018-11-20 15:53:26 +0300

    1

--- Lesson6/script.js
+++ Lesson6/script.js
@@ -6,21 +6,6 @@ var User = function (firstName, lastName, regDate) {
   this.regDate = regDate;
 };
 
-// var UserList = function(user) {
-//   var users = [];
-//   var addUser = function(user) {
-//     users.push(user);
-//   };
-//   var getAllUsers = function() {
-//     return users;
-//   };
-// };
-
-function createUser(input) {
-  var u = input;
-  return u;
-}
-
 var date = new Date();
 
 var users = [];
