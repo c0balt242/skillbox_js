8a90ddb2ff105b1d2861483491263ac24c7f4cf6
c0balt242 <43532677+c0balt242@users.noreply.github.com>
2018-11-20 22:31:40 +0300

    1

--- Lesson6/script.js
+++ Lesson6/script.js
@@ -1,4 +1,4 @@
-"use strict";
+//"use strict";
 
 var User = function(firstName, lastName, regDate) {
   this.firstName = firstName;
@@ -15,18 +15,24 @@ var UserList = function() {
 
   this.show = function() {
     for (var i = 0; i <= this.users.length; i++) {
-      document.write(this.users[i].firstName + ' ' + this.users[i].lastName + ' ' + this.users[i].regDate + '<br>');
+      console.log(this.users[i].firstName + ' ' + this.users[i].lastName + ' ' + this.users[i].regDate + '<br>');
     }
   };
 };
 
 var date = new Date();
 var list = new UserList();
+// var user = new User("Anatoliy", "Ivanov", date.toString());
+// list.add(user);
+// var user = new User("Petya", "Kakashkin", date.toString());
+// list.add(user);
+// var user = new User('Vasya', 'Srakin', date.toString());
+// list.add(user);
 
-var user = new User("Anatoliy", "Ivanov", date.toString());
+var input = prompt('Enter first and last name: ');
+var str = input.split(' ', 2);
+var user = new User(str[0], str[1], date.toDateString());
 list.add(user);
-var user = new User("Petya", "Kakashkin", date.toString());
-list.add(user);
-var user = new User('Vasya', 'Zalupkin', date.toString());
-list.add(user);
+
 console.log(list.show());
+
