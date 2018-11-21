e5bc3ab4671b75c0c16bdc74094e3e453b04b6ce
c0balt <c0balt@yandex.ru>
2018-11-21 17:54:39 +0300

    1

--- Lesson5/lib.js
+++ Lesson5/lib.js
@@ -1,37 +1,51 @@
 var start = (function () {
 
-  var random = Math.round(Math.random() * 1000);
-  var input = 0;
-  var count = 10;
-
-  function check(input) {
-    return !input || isNaN(input);
-  }
-
-  function compare(input, random) {
-    if (random < input) {
-      alert('Это число меньше. Осталось попыток: ' + count);
-      return false;
-    } else if (random > input) {
-      alert('Это число больше. Осталось попыток: ' + count);
-      return false;
-    } else {
-      alert('Вы угадали! Количество попыток: ' + (10 - count));
-      return true;
-    }
-  }
-
   return function () {
-    while (input != null) {
+    var count = 10;
+    var random = Math.round(Math.random() * 1000);
+    var input;
+
+    // function isNull(input) {
+    //   return input === null;
+    // }
+    var isNull = input => input === null;
+
+    // function check(input) {
+    //   return !input || isNaN(input);
+    // }
+
+    var check = input => !input || isNaN(input);
+
+    function compare(input, random) {
+      if (random < input) {
+        alert('Загаданное число меньше. Осталось попыток: ' + count);
+        return false;
+      } else if (random > input) {
+        alert('Загаданное число больше. Осталось попыток: ' + count);
+        return false;
+      } else {
+        alert('Вы угадали! Количество попыток: ' + (10 - count));
+        return true;
+      }
+    }
+
+    function restart() {
+      var tr = confirm('Попытки закончились. Хотите сыграть ещё раз?');
+      if (tr) {
+        return start();
+      }
+    }
+    while (!isNull(input)) {
       input = prompt('Угадайте число от 1 до 1000: ');
-      if (input == null) {
+      if (isNull(input)) {
         alert('Игра окончена.');
         break;
       }
       count--;
       if (count === 0) {
-        alert('Попытки закончились');
-        break;
+        if (!restart()) {
+          break;
+        }
       } else if (check(input)) {
         alert('Это не число!');
       } else if (compare(input, random)) {
