51f653d5df6a08060f4a2df9a340a296013ba7fc
c0balt <c0balt@yandex.ru>
2018-11-23 10:34:44 +0300

    1

--- Lesson6/script.js
+++ Lesson6/script.js
@@ -1,4 +1,5 @@
-//"use strict";
+// "use strict";
+// debugger
 
 var User = function(firstName, lastName, regDate) {
   this.firstName = firstName;
@@ -9,14 +10,15 @@ var User = function(firstName, lastName, regDate) {
 var UserList = function() {
   this.users = [];
 
-  this.add = function() {
+  this.add = function(user) {
     this.users.push(user);
+    console.log('adding user ' + user.firstName);
   };
 
   this.show = function() {
-    // for (var i = 0; i <= this.users.length; i++) {
-    //   console.log(this.users[i].firstName + " " + this.users[i].lastName + " " + this.users[i].regDate + "<br>");
-    // }
+    for (var i = 0; i < this.users.length; i++) {
+      console.log(this.users[i].firstName + " " + this.users[i].lastName + " " + this.users[i].regDate);
+    }
   };
 };
 
@@ -28,13 +30,23 @@ var date = new Date();
 var list = new UserList();
 // var user = new User("Anatoliy", "Ivanov", date.toString());
 // list.add(user);
-// var user = new User("Petya", "Kakashkin", date.toString());
+// var user = new User("Vasya", "Grechushev", date.toString());
 // list.add(user);
-// var user = new User('Vasya', 'Srakin', date.toString());
+// var user = new User("Andrey", "Sagitov", date.toString());
 // list.add(user);
 
-var input = prompt("Enter first and last name: ");
-var str = input.split(" ", 2);
-var user = new User(str[0], str[1], date.toDateString());
-list.add(user);
-console.log(list.show());
+
+function addUser() {
+  var input = prompt("Enter first and last name: ");
+  var str = input.split(" ", 2);
+  var user = new User(str[0], str[1], date.toDateString());
+  list.add(user);
+}
+
+addUser();
+addUser();
+addUser();
+
+list.show();
+
+// console.log(list.show());
