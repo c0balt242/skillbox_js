d85066b166445065636c3280176369c763414f03
c0balt242 <43532677+c0balt242@users.noreply.github.com>
2018-11-23 20:53:27 +0300

    1

--- Lesson6/script.js
+++ Lesson6/script.js
@@ -6,7 +6,7 @@ var User = function(firstName, lastName, regDate) {
   this.regDate = regDate;
 };
 
-var UserList = function() {
+var UserList = function(User) {
   this.users = [];
 
   this.add = function() {
@@ -14,27 +14,27 @@ var UserList = function() {
   };
 
   this.show = function() {
-    // for (var i = 0; i <= this.users.length; i++) {
-    //   console.log(this.users[i].firstName + " " + this.users[i].lastName + " " + this.users[i].regDate + "<br>");
-    // }
+    for (var i = 0; i <= this.users.length; i++) {
+      console.log(this.users[i].firstName + " " + this.users[i].lastName + " " + this.users[i].regDate + "<br>");
+    }
   };
 };
 
-function isNull(input) {
-  return input === null;
-}
+// function isNull(input) {
+//   return input === null;
+// }
 
 var date = new Date();
 var list = new UserList();
-// var user = new User("Anatoliy", "Ivanov", date.toString());
-// list.add(user);
-// var user = new User("Petya", "Kakashkin", date.toString());
-// list.add(user);
-// var user = new User('Vasya', 'Srakin', date.toString());
-// list.add(user);
-
-var input = prompt("Enter first and last name: ");
-var str = input.split(" ", 2);
-var user = new User(str[0], str[1], date.toDateString());
+var user = new User("Anatoliy", "Ivanov", date.toString());
 list.add(user);
+var user = new User("Petya", "Kakashkin", date.toString());
+list.add(user);
+var user = new User('Vasya', 'Srakin', date.toString());
+list.add(user);
+
+// var input = prompt("Enter first and last name: ");
+// var str = input.split(" ", 2);
+// var user = new User(str[0], str[1], date.toDateString());
+// list.add(user);
 console.log(list.show());
